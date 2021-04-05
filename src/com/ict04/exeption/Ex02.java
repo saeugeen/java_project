938a90f4519d2d85a4db70c22b70add8b7f34540
saeugeen <eugeen7083@gmail.com>
2021-04-05 16:17:39 +0900

    exeption02

---
+++ src/com/ict04/exeption/Ex02.java
@@ -0,0 +1,14 @@
+package com.ict04.exeption;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		try {
+			String year ="2002년";
+			int age = 2021 - Integer.parseInt(year);
+			System.out.println("나이: " + age);
+		} catch (Exception e) {
+			System.out.println("숫자만 입력하세요");
+		}
+		System.out.println("수고 하셨습니다.");
+	}
+}
