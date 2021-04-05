9e0b70e4a49e2a823ce54cb57d459f38067e0530
saeugeen <eugeen7083@gmail.com>
2021-04-05 16:10:43 +0900

    exection01

--- src/com/ict04/exeption/Ex01.java
+++ src/com/ict04/exeption/Ex01.java
@@ -2,7 +2,29 @@ package com.ict04.exeption;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		
-		//주석
+		//예외 처리 : Exception
+		//			  try ~ catch
+		//목적 : 비정상적인 종료를 정상적인 종료로 유도,
+		//		 예외 발생 시 예외에 대한 처리를 해준다.
+		//형식 : try{
+		//			예외가 발생할 수 있는 문장;
+		//			예외가 발생할 수 있는 문장;
+		//			예외가 발생할 수 있는 문장;
+		//			}catch(예외발생객체 e){
+		//			예외가 발생할 수 있는 문장;
+		//			}
+		try {
+			int[] var = {10,20,30};
+			for (int i = 0; i <= var.length; i++) {
+				System.out.println(1);
+				System.out.println(2);
+				System.out.println("var["+i+"]=" +var[i]);
+				System.out.println(3);
+				System.out.println(4);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		System.out.println("수고 하셨습니다.");
 	}
 }
