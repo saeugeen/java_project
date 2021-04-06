96aa38d151128c6388e753e721cf02d08078eee2
saeugeen <eugeen7083@gmail.com>
2021-04-06 14:19:42 +0900

    collection06

---
+++ src/com/ict05/collection/Ex06.java
@@ -0,0 +1,13 @@
+package com.ict05.collection;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		// List 인터페이스를 구현한 클래스들 : stack ,ArrayList, Vector
+		// ArrayList와 Vector : 배열과 흡사한 구조이지만, 삽입,삭제, 추가가 자유롭다.
+		//						크기를 미리 지정하지 않아도 된다.
+		//						ArrayList(동기화 지원 안함)와 Vector(동기화 지원)
+		// Set 구조는 순서가 없다. 중복 안됨
+		// List 구조는 순서가 있다. 중복 가능
+		
+	}
+}
