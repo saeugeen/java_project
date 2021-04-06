63e91e1e475bc32deaeb2f8b3ef5f89836114799
saeugeen <eugeen7083@gmail.com>
2021-04-06 09:21:44 +0900

    baseball

--- src/com/ict03/class02/test.java
+++ src/com/ict03/class02/test.java
@@ -1,21 +1,57 @@
 package com.ict03.class02;
 
+import java.util.Scanner;
+
 public class test {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int com[] = new int[3];
 		int my[] = new int[3];
+
+		int time = 10;
 		
-		int strike = 0;
-		int ball = 0;
-		
-		
-		
-		for (int i = 0; i < com.length; i++) {
-			com[i] = (int)(Math.random()*10)+1;
+		for (int i = 0; i < 3; i++) {
+			com[i] = (int) (Math.random() * 10) + 1;
+			for (int j = 0; j < i; j++) {
+				if (com[i] == com[j]) {
+					i--;
+					break;
+				}
+			}
 		}
-		for (int i = 0; i < my.length; i++) {
-			
+
+		while (time > 0) {
+			int strike = 0;
+			int ball = 0;
+			System.out.print("숫자를 입력하세요");
+			for (int i = 0; i < 3; i++) {
+				my[i] = sc.nextInt();
+			}
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (com[i] == my[j]) {
+						if (i == j) {
+							strike++;
+						} else {
+							ball++;
+						}
+					}
+				}
+			}
+			if (strike == 3) {
+				System.out.println("이겼습니다.");
+				break;
+			} else {
+				time--;
+				if (time == 0) {
+					System.out.println("기회 끝남");
+
+				} else {
+					System.out.println(strike + "스트라이크" + ball + "볼");
+				}
+			}
 		}
 		
+
 	}
 }
