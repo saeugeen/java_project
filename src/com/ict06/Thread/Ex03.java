8171ece6d4cc37311a0177518c4949ee9f864966
saeugeen <eugeen7083@gmail.com>
2021-04-07 10:06:59 +0900

    Thread03

--- src/com/ict06/Thread/Ex03.java
+++ src/com/ict06/Thread/Ex03.java
@@ -9,15 +9,22 @@ package com.ict06.Thread;
 //Runnable 인터페이스는 run()만 가지고 있다.
 
 public class Ex03 extends Thread{
+
+	public void go() {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(Thread.currentThread().getName()+":"+i);
+		}
+	}
 	
+	/*
 	@Override
 	public void start() {
 		for (int i = 1; i < 10; i++) {
 			System.out.println(Thread.currentThread().getName()+":"+i);
 		}
 	}
-	
-	
+	*/
+
 	@Override
 	public void run() {
 		for (int i = 1; i < 10; i++) {
