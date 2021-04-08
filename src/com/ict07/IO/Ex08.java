a9b1acf24f7d61e3d05bca1fc53e70e0746dfa51
saeugeen <eugeen7083@gmail.com>
2021-04-08 14:01:16 +0900

    IO08

--- src/com/ict07/IO/Ex08.java
+++ src/com/ict07/IO/Ex08.java
@@ -1,66 +1,81 @@
 package com.ict07.IO;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Ex08 {
 	public static void main(String[] args) {
-		//속도 향상을 위해서 BufferedOutputStream을 사용한다.
-		//혼자서는 사용할수 없고 반드시 OutputStream을 받아서 사용
-		//생성자 : BufferedOutStream(OutoutStream out)
+		// 속도향상을 위해서 BufferedInputStream를 사용한다.
+		// 혼자서는 사용할 수 없고 반드시 InputStream를 받아서 사용
+		// 생성자 : BufferedInputStream(InputStream in)
 		
-		// 특정위치의 파일까지 지정
-				String pathname = "C:"+File.separator+"study"+File.separator
-									+"util"+File.separator+"ict01.txt";
-				File file = new File(pathname);
-				FileOutputStream fos = null;
-				BufferedOutputStream bos = null;
-				try {
-					// 기존파일이 있으면 덮어쓰기 하고, 없으면 생성한다.
-					fos = new FileOutputStream(file);
-					bos = new BufferedOutputStream(fos);
-					
-					// 기존파일이 있으면 이어쓰기 하고, 없으면 생성한다.
-					// fos = new FileOutputStream(file, true);
-					
-					// 한글자 쓰기 
-					bos.write(65); //A
-					bos.write(97); //a
-					
-					//int이기 때문에 char를 사용할 수 있다.
-					bos.write('j');
-					bos.write('a');
-					bos.write('v');
-					bos.write('a');
-					
-					//자바에서 줄변경은 13(Carriage return =CR =\r)이다.
-					//다른 언어중에는  (\r\n(Line feed = 10)을 같이 사용하는 경우도 있다.)
-					bos.write(13);
-					
-					//배열을 이용해서 여러글자 쓰기
-					byte[] b = {'H','i','J','A','V','A',13};
-					bos.write(b);
-					
-					//String을 이용하면(영어이외 문자도 가능)
-					String str = "hello\r안녕\r123\r大韓";
-					byte[] b2 =str.getBytes();
-					bos.write(b2);
-
-					bos.flush();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						bos.close();
-						fos.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
+		String pathname = "C:"+File.separator+"study"+File.separator+"util"
+							+ File.separator+"ict01.txt";
+		File file = new File(pathname);
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		try {
+		    fis = new FileInputStream(file);
+		    bis = new BufferedInputStream(fis);
+		    // 한글자 읽어오기 : read() => 숫자(아스키코드) -> 형변환
+		    //int k = bis.read();
+		    //System.out.println(k + ":" + (char)(k));
+		    
+		    // 한글자씩 계속 읽기 : 그림, 소리, 동영상, 파일을 무조건 해당 방식을 사용
+		    //                      비 영어권인 문자들은 읽을 수 없다.
+		    /*
+		    int k = 0 ;
+		    while(true) {
+		    	k = bis.read();
+		    	if(k == -1) break;
+		    	System.out.println(k+":"+(char)(k));
+		    }
+		    */
+		    /*
+		    int k = 0 ;
+		    // fis을 이용해서 한 글자을 읽었을 때 -1과 같지 않으면 실행해라
+		    while((k=bis.read())!= -1) {
+		    	System.out.println(k+":"+(char)(k));
+		    }
+		   */
+		    
+		    // 배열을 이용하는 방법 : 비영어권 사용 못한
+		    
+		    byte[] b = new byte[(int)(file.length())];
+		    bis.read(b);
+		    for (byte k : b) {
+				System.out.println(k+":"+(char)(k));
+			}
+		    
+		 // String를 이용하자(영어이외 문자도 가능)
+		 // String 생성자에는 byte[] 인자로 받는 생성자를 이용하면 된다.
+		 /*   
+		    byte[] b = new byte[(int)(file.length())];
+		    fis.read(b);
+		    String str = new String(b);
+		    System.out.println(str);
+		   */ 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bis.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
+	}
+}
+
+
+
+
+
+
+
